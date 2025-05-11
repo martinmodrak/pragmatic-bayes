9eeacd9af72c8be54a857c2c45f14ceef57dfe8f
Martin Modrák <modrak.mar@gmail.com>
2025-05-11 23:46:29 +0200

    First talk WIP version

---
+++ group_ci.R
@@ -0,0 +1,49 @@
+# A relatively stupid exploration based on https://stats.stackexchange.com/a/662565/73129
+# Need to fix/handle convergence errors
+
+library(MASS)
+library(dplyr)
+library(ggplot2)
+library(brms)
+options(brms.backend = "cmdstanr", mc.cores = 4)
+
+mu_0 <- log(100)
+group_eff <- log(1.5)
+true_theta <- 2.5
+group <- c(rep(0, 4), rep(1, 4))
+# proof of concept
+y <- rnegbin(8, mu = exp(mu_0 + group_eff * group), 
+             theta = true_theta)
+confint(glm.nb(y ~ group))
+confint(glm.nb(y ~ group))[2, ]
+
+fit_brm <- brms::brm(y ~ group, data = data.frame(y, group), family = "negbinomial")
+fixef(fit_brm)["group", c(3,4)]
+# simulation
+nsim <- 100
+# calculates relatively fast, but spams messages
+res <- t(replicate(nsim, {
+  y <- rnegbin(8, mu = exp(mu_0 + group_eff * group), 
+               theta = true_theta)
+  cf_freq <- suppressMessages(confint(glm.nb(y ~ group))[2, ])
+  fit_brm <- brms::brm(y ~ group, data = data.frame(y, group), family = "negbinomial", refresh = 0)
+  cf_brm <- fixef(fit_brm)["group", c(3,4)]
+  c(cf_freq, cf_brm)
+})
+)
+
+dat <- data.frame(res, group_eff, i = 1:nsim)
+# just for fun
+ggplot(dat, aes(y = rank(`X2.5..`), x = group_eff)) +
+  geom_linerange(aes(xmin = `X2.5..`, xmax = `X97.5..`, 
+                     color = group_eff < X97.5.. & group_eff > X2.5..)) +
+  geom_point(color = "red")  
+
+ggplot(dat, aes(y = rank(Q2.5), x = group_eff)) +
+  geom_linerange(aes(xmin = Q2.5, xmax = Q97.5, 
+                     color = group_eff <  Q97.5 & group_eff > Q2.5)) +
+  geom_point(color = "red")  
+
+# about 86% coverage for the 95%-CI
+dat %>% filter(group_eff < X97.5.. & group_eff > X2.5..) %>%  count()/nsim
+dat %>% filter(group_eff < Q97.5 & group_eff > Q2.5) %>%  count()/nsim
