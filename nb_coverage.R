1ab7e450201a0100a0501c7d286a368e17826152
Martin Modrák <modrak.mar@gmail.com>
2025-05-14 15:05:04 +0200

    Added gamlss simulations for nb

--- nb_coverage.R
+++ nb_coverage.R
@@ -11,21 +11,28 @@ sim_nb_coverage_single <- function(N_per_group, mu, b, phi, sim_id, base_brm) {
   
   cf_glm.nb <- suppressMessages(confint(MASS::glm.nb(y ~ group, data = data))[2, ])
   
+  cf_gamlss <- tryCatch({
+  fit_gamlss <- suppressMessages(gamlss::gamlss(y ~ group, family = "NBI"))
+  confint(fit_gamlss)[2,]
+  }, error = function(e) {return(c(NA,NA))})
+  
   # Rarely we get problematic init which results in fitted phi -> infty and bad BFMI 
   # refitting fixes that (more informative prior on phi also would)
   for(i in 1:5) {
-    fit_brm <- update(base_brm, newdata = data, cores = 1, chains = 2, future = FALSE, backend = "cmdstanr")
-    
+    fit_brm <- update(base_brm, newdata = data, cores = 1, chains = 2, future = FALSE, backend = "cmdstanr", refresh = 0)
+
     if(all(!is.na(rstan::get_bfmi(fit_brm$fit)))) {
       break
     }
   }
   bfmi_problem <- any(is.na(rstan::get_bfmi(fit_brm$fit)))
   cf_brm <- brms::fixef(fit_brm)["group", c(3,4)]
-  
-  data.frame(method = c("glm.nb", "brms"), sim_id, mu = mu, b = b, phi = phi, N_per_group = N_per_group,
-             ci_low = unname(c(cf_glm.nb[1], cf_brm[1])), ci_high = unname(c(cf_glm.nb[2], cf_brm[2])),
-             bfmi_problem = c(FALSE, bfmi_problem)
+  # bfmi_problem <- FALSE
+  # cf_brm <- c(NA,NA) 
+
+  data.frame(method = c("glm.nb", "gamlss", "brms"), sim_id, mu = mu, b = b, phi = phi, N_per_group = N_per_group,
+             ci_low = unname(c(cf_glm.nb[1], cf_gamlss[1], cf_brm[1])), ci_high = unname(c(cf_glm.nb[2], cf_gamlss[2], cf_brm[2])),
+             bfmi_problem = c(FALSE, FALSE, bfmi_problem)
   )
 }
 
@@ -43,12 +50,10 @@ prior <- c(
 base_brm <- brms::brm(y ~ group, data = data.frame(y, group), family = "negbinomial", prior = prior, backend = "cmdstanr")
 
 
-scenarios <- tidyr::crossing(N_per_group = 4, mu = log(100), b = seq(0, 2, length.out = 10), phi = c(0.5, 1, 2.5, 10), sim_id = 1:200)
+scenarios <- tidyr::crossing(N_per_group = 4, mu = log(100), b = seq(0, 2, length.out = 10), phi = c(0.5, 1, 2.5, 10), sim_id = 1:1000)
 #scenarios <- crossing(N_per_group = 4, mu = log(100), b = seq(0, 2, length.out = 5), phi = c(1, 2.5), sim_id = 1:2)
 
 future::plan(future::multisession)
-#future::plan(future.mirai::mirai_multisession(workers = 6))
-#future::plan(future::sequential)
 
 nb_coverage_df <- furrr::future_pmap_dfr(scenarios, \(...) sim_nb_coverage_single(..., base_brm = base_brm), .options = furrr::furrr_options(seed = TRUE, chunk_size = 40))
 
